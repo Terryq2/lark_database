03da0c80f667e316e7cbabd15c44e957b6083737
Terry Qiu <terryq2@illinois.edu>
2025-08-04 13:30:27 +0800

    Slightly cleaner main.py

--- main.py
+++ main.py
@@ -7,27 +7,22 @@ import os
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 from src.driver import DataSyncClient
-from src.config import FinancialQueries
 
-def job():
-    _job_for_cinema_tickets()
-    _job_for_others()
-    _message_after_job()
+def job(syncer: DataSyncClient):
+    _job_for_cinema_tickets(syncer)
+    _job_for_others(syncer)
+    _message_after_job(syncer)
 
-
-def _job_for_cinema_tickets():
-    syncer = DataSyncClient(".env", "config.json")
+def _job_for_cinema_tickets(syncer: DataSyncClient):
     syncer.sync_most_recent_data('C01', syncer.config.get_name('C01'))
 
 
-def _job_for_others():
-    syncer = DataSyncClient(".env", "config.json")
+def _job_for_others(syncer: DataSyncClient):
     syncer.sync_all_yesterday()
     syncer.sync_screening_data()
 
 
-def _message_after_job():
-    syncer = DataSyncClient(".env", "config.json")
+def _message_after_job(syncer: DataSyncClient):
     current_time = datetime.now()
     timestamp = current_time.strftime("%Y-%m-%d %H:%M:%S,%f")[:-3]
 
@@ -40,8 +35,7 @@ def _message_after_job():
         syncer.lark_client.get_chat_group_id_by_name('服务器状态')
     )
 
-def _message_chat():
-    syncer = DataSyncClient(".env", "config.json")
+def _message_ok(syncer: DataSyncClient):
     current_time = datetime.now()
     timestamp = current_time.strftime("%Y-%m-%d %H:%M:%S,%f")[:-3]
 
@@ -54,20 +48,33 @@ def _message_chat():
         syncer.lark_client.get_chat_group_id_by_name('服务器状态')
     )
 
+def _message_init(syncer: DataSyncClient):
+    current_time = datetime.now()
+    timestamp = current_time.strftime("%Y-%m-%d %H:%M:%S,%f")[:-3]
+
+    message = json.dumps({
+        "text": (f'{timestamp}' f' <b>SERVER INITIALIZING</b>')
+    })
+
+    syncer.lark_client.send_message_to_chat_group(
+        message,
+        syncer.lark_client.get_chat_group_id_by_name('服务器状态')
+    )
 
 if __name__ == "__main__":
+    global_syncer = DataSyncClient(".env", "config.json")
     try:
-        _message_chat()
+        _message_init(global_syncer)
         scheduler = BlockingScheduler()
 
-        scheduler.add_job(job, 'cron', hour=8, minute=0)
-        scheduler.add_job(
-            _message_chat,
-            'cron',  # Runs at :00 and :30 every hour
-            hour='0-23/4'     # Runs every 4 hours (0-23)
-        )
+        scheduler.add_job(job, 'cron', hour=8, minute=0, args=[global_syncer])
+
+        # Use lambda or partial to defer execution
+        scheduler.add_job(_message_ok, 'cron', hour='0-23/4', args=[global_syncer])
+
         scheduler.start()
     except Exception as e:
         os._exit(1)
+
         
 
